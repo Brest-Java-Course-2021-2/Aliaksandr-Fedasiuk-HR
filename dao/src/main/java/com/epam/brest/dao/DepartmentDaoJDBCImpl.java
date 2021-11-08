dbf0ee91117409678e5556a98842e2128357e753
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-11-08 20:05:03 +0300

    test-db module, create/init department table

--- dao/src/main/java/com/epam/brest/dao/DepartmentDaoJDBCImpl.java
+++ dao/src/main/java/com/epam/brest/dao/DepartmentDaoJDBCImpl.java
@@ -1,10 +1,18 @@
 package com.epam.brest.dao;
 
 import com.epam.brest.model.Department;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 public class DepartmentDaoJDBCImpl implements DepartmentDao {
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public DepartmentDaoJDBCImpl(DataSource dataSource) {
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     @Override
     public List<Department> findAll() {
---
+++ test-db/src/main/resources/init-db.sql
@@ -0,0 +1,3 @@
+INSERT INTO department(departmentId, departmentName) VALUES (1, 'IT');
+INSERT INTO department(departmentId, departmentName) VALUES (2, 'ACCOUNTING');
+INSERT INTO department(departmentId, departmentName) VALUES (3, 'MANAGEMENT');
