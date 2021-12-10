339cbd7b84c4eb600c49f2b86a20c63ef9e16927
Siarhei Berdachuk <berdachuk@berdaflex.com>
2021-12-10 20:28:01 +0300

    add separate spring boot applications

--- rest-app/src/main/java/com/epam/brest/RestApplication.java
+++ rest-app/src/main/java/com/epam/brest/RestApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:dao.properties"})
 public class RestApplication extends SpringBootServletInitializer {
 
     private static final Logger logger = LogManager.getLogger(RestApplication.class);
