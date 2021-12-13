bf729f73abe29ccbffdab721754e5df0a810276e
Siarhei Berdachuk <berdachuk@berdaflex.com>
2021-12-13 20:42:09 +0300

    CORS fix

--- rest-app/src/main/java/com/epam/brest/rest/DepartmentDtoController.java
+++ rest-app/src/main/java/com/epam/brest/rest/DepartmentDtoController.java
@@ -5,12 +5,14 @@ import com.epam.brest.model.dto.DepartmentDto;
 import com.epam.brest.service.DepartmentDtoService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
+@CrossOrigin
 public class DepartmentDtoController {
 
     private static final Logger logger = LogManager.getLogger(DepartmentDaoJDBCImpl.class);
